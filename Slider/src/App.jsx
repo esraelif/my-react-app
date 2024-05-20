584cc95203ac681665d78cfbffd463817e8bb45d
esraelif <elifesratunca@gmail.com>
2024-05-20 16:36:40 +0200

    carousel

---
+++ Slider/src/App.jsx
@@ -0,0 +1,18 @@
+// import Carousel from "./Carousel"
+
+import SlickCarousel from "./SlickSlider"
+
+
+function App() {
+
+
+  return (
+    <main>
+      {/* <Carousel/> */}
+      <SlickCarousel />
+
+    </main>
+  )
+}
+
+export default App
