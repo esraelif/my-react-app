423a267ea2d32240deb3c40dfa7aea75d93b6a06
esraelif <elifesratunca@gmail.com>
2024-05-27 19:42:56 +0200

    strapi

--- strapi-submenus/src/Context.jsx
+++ strapi-submenus/src/Context.jsx
@@ -5,13 +5,14 @@ const AppContext = createContext()
 
 export const AppProvider = ({ children }) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+    const [pageId, setPageId] = useState(null)
     const openSidebar = () => {
         setIsSidebarOpen(true)
     };
     const closeSidebar = () => {
         setIsSidebarOpen(false)
     };
-    return <AppContext.Provider value={{ isSidebarOpen, setIsSidebarOpen, openSidebar, closeSidebar }}>
+    return <AppContext.Provider value={{ isSidebarOpen, setIsSidebarOpen, openSidebar, closeSidebar, pageId, setPageId }}>
         {children}
     </AppContext.Provider>
 }
